2166c1251429240115cd019ee38be0e1875664e2
ura47 <150671277+ura47@users.noreply.github.com>
2025-11-01 10:52:40 +0400

    Create bot_runner.py

---
+++ bot_runner.py
@@ -0,0 +1,25 @@
+import os
+import telebot
+
+bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
+
+COMMANDS = {
+    "about": "https://t.me/finansmost/26",
+    "begin": "https://t.me/finansmost/31",
+    "2fa": "https://t.me/finansmost/44", 
+    "verifikaciya": "https://t.me/finansmost/41",
+    "birga": "https://t.me/finansmost/41",
+    "p2p": "https://t.me/finansmost/49",
+    "portfel": "https://t.me/finansmost/23",
+    "moneti": "https://t.me/finansmost/52",
+    "strportfel": "https://t.me/finansmost/55",
+    "tools": "https://t.me/finansmost/63"
+}
+
+@bot.message_handler(commands=list(COMMANDS.keys()))
+def send_answer(message):
+    command = message.text[1:]
+    bot.send_message(message.chat.id, COMMANDS[command])
+
+print("Бот запущен...")
+bot.polling()
